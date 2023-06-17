2aa67af95bc3545f63a7498b5a94da25978e500d
HatePK <hatepk@gmail.com>
2023-06-17 20:59:53 +0300

    9 sprint 1 iteration

--- app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SettingsActivity.kt
@@ -2,9 +2,11 @@ package com.practicum.playlistmaker
 
 import android.annotation.SuppressLint
 import android.content.Intent
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
+import android.widget.FrameLayout
 
 class SettingsActivity : AppCompatActivity() {
     @SuppressLint("MissingInflatedId")
@@ -16,6 +18,36 @@ class SettingsActivity : AppCompatActivity() {
         back.setOnClickListener {
             val searchIntent = Intent(this, MainActivity::class.java)
             startActivity(searchIntent)
+        }
+
+        val share = findViewById<FrameLayout>(R.id.share_button)
+        share.setOnClickListener {
+            val shareIntent = Intent().apply {
+                action = Intent.ACTION_SEND
+                putExtra(Intent.EXTRA_TEXT, getString(R.string.share_content))
+                type = "text/plain"
+            }
+            startActivity(shareIntent)
+        }
+
+        val support = findViewById<FrameLayout>(R.id.support_button)
+        support.setOnClickListener {
+            val supportIntent = Intent(Intent.ACTION_SENDTO)
+
+            supportIntent.data = Uri.parse(
+                getString(R.string.support_extra_email) +
+                    "?subject=${getString(R.string.support_extra_topic)}" +
+                    "&body=${getString(R.string.support_extra_text)}"
+            )
+
+            startActivity(Intent.createChooser(supportIntent, getString(R.string.support_extra_topic)))
+        }
+
+        val agreement = findViewById<FrameLayout>(R.id.agreement_button)
+        agreement.setOnClickListener {
+            val agreementIntent = Intent(Intent.ACTION_VIEW)
+            agreementIntent.data = Uri.parse(getString(R.string.agreement_link))
+            startActivity(agreementIntent)
         }
     }
 }
