285314a115e50ed3398f4139c1d5b62e28ded1a8
HatePK <hatepk@gmail.com>
2023-10-31 01:26:04 +0300

    19 sprint

---
+++ app/src/main/java/com/practicum/playlistmaker/root/RootActivity.kt
@@ -0,0 +1,23 @@
+package com.practicum.playlistmaker.root
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.practicum.playlistmaker.R
+
+class RootActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_root)
+
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.rootFragmentContainerView) as NavHostFragment
+        val navController = navHostFragment.navController
+
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
+        bottomNavigationView.setupWithNavController(navController)
+    }
+
+}
